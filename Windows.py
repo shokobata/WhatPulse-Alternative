c0b661d7472d5df2a264b635c0ae49ae63470010
shokobata <98646418+shokobata@users.noreply.github.com>
2022-02-04 19:20:22 +0300

    Added some comments

--- Windows.py
+++ Windows.py
@@ -1,7 +1,7 @@
-# last edited 2-2-2022
+# last edited 4-2-2022
 import PySimpleGUI as sg
 import json
-# Current: Add a button to refresh/update data
+
 sg.theme('DarkAmber')
 
 StatsOpen = False
@@ -9,10 +9,10 @@ StatsOpen = False
 def StartStatsWindow():
     global StatsOpen
 
-    if StatsOpen == False:
+    if StatsOpen == False: # check if the window is already open
         StatsOpen = True
 
-        with open("Data.json") as File:
+        with open("Data.json") as File: # read the data from the file and assign variables to it
             Data = json.loads(File.read())
             LeftMouseClicks = Data["Left Clicks"]
             RightMouseClicks = Data["Right Clicks"]
@@ -20,7 +20,8 @@ def StartStatsWindow():
             MouseScrolls = Data["Scrolls"]
             KeyPresses = Data["Key Presses"]
 
-        col1=[[sg.Image("Mouse.png")],
+        # the layout of the window
+        col1=[[sg.Image("Mouse.png")], 
                 [sg.Text("Left Clicks: "+str(LeftMouseClicks), key="_LeftClicks_", size=(18,1))],
                 [sg.Text("Right Clicks: "+str(RightMouseClicks), key="_RightClicks_", size=(18,1))],
                 [sg.Text("Middle Clicks: "+str(MiddleMouseClicks), key="_MiddleClicks_", size=(18,1))],
@@ -33,22 +34,23 @@ def StartStatsWindow():
             [sg.Column(col1, element_justification='c', justification="center"), sg.Column(col2, element_justification='c', justification="center")]
         ]
 
-        window = sg.Window('WhatPulse Alternative', layout, use_default_focus=False)
+        window = sg.Window('WhatPulse Alternative', layout, use_default_focus=False) # showing the window
 
         while True:
             event, values = window.read()
-            if event == sg.WIN_CLOSED:
+            if event == sg.WIN_CLOSED: # check if the window is closed
                 StatsOpen = False
                 break
-            if event == "Refresh":
-                with open("Data.json") as File:
+            if event == "Refresh": # check if the refresh button is pressed
+                with open("Data.json") as File: # read the data from the file and assign variables to it
                     Data = json.loads(File.read())
                     LeftMouseClicks = Data["Left Clicks"]
                     RightMouseClicks = Data["Right Clicks"]
                     MiddleMouseClicks = Data["Middle Clicks"]
                     MouseScrolls = Data["Scrolls"]
                     KeyPresses = Data["Key Presses"]
-                window["_KeyPresses_"].update("Key Presses: "+str(KeyPresses))
+                # updating the window text
+                window["_KeyPresses_"].update("Key Presses: "+str(KeyPresses)) 
                 window["_LeftClicks_"].update("Left Clicks: "+str(LeftMouseClicks))
                 window["_MiddleClicks_"].update("Middle Clicks: "+str(MiddleMouseClicks))
                 window["_RightClicks_"].update("Right Clicks: "+str(RightMouseClicks))
