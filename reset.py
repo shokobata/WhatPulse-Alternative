0dd4f9e19a5bb868657059507defaa241cf3e3c2
Misanthropik <115428022+Misanthropik@users.noreply.github.com>
2022-10-11 22:58:47 +0530

    Added new functions.
    * Display Daily and LIfetime keyboard usage table.
    
    Got rid of 'CMD' restarting behavior.
    Cleaned up the code.

--- reset.py
+++ reset.py
@@ -1,95 +1,95 @@
 # last edited 10-10-2022
-import os, time, json
-from sys import exit
+import os, json
+import PySimpleGUI as sg
 from datetime import date
+from re import sub as sub
 
-datevar = date.today()
-todaydate = str(datevar)
-with open('DailyData.json', encoding='utf8') as loaddate:
-    myDict = json.load(loaddate)
-    datescope = myDict["Date"]
-    if todaydate != datescope: # If Date is not concurrent, Updates Date key to today and resets all daily values to 1;
-        with open('DailyData.json', encoding='utf8') as datereset:
-            myDictDate = json.load(datereset)
-            myDictDate["Date"] = todaydate
-        with open('DailyData.json', 'w') as datereset:
-            datereset.write(json.dumps(myDictDate, indent=4))
-        with open('DailyData.json', encoding='utf8') as resetfile:
-            myDict = json.load(resetfile)
-            myDict["Today's LClicks"] = 1
-            myDict["Today's RClicks"] = 1
-            myDict["Today's MClicks"] = 1
-            myDict["Today's Scrolls"] = 1
-            myDict["Today's KeyPress"] = 1
-            myDict["Today's letter"]['a'] = 1
-            myDict["Today's letter"]['b'] = 1
-            myDict["Today's letter"]['c'] = 1
-            myDict["Today's letter"]['d'] = 1
-            myDict["Today's letter"]['e'] = 1
-            myDict["Today's letter"]['f'] = 1
-            myDict["Today's letter"]['g'] = 1
-            myDict["Today's letter"]['h'] = 1
-            myDict["Today's letter"]['i'] = 1
-            myDict["Today's letter"]['j'] = 1
-            myDict["Today's letter"]['k'] = 1
-            myDict["Today's letter"]['l'] = 1
-            myDict["Today's letter"]['m'] = 1
-            myDict["Today's letter"]['n'] = 1
-            myDict["Today's letter"]['o'] = 1
-            myDict["Today's letter"]['p'] = 1
-            myDict["Today's letter"]['q'] = 1
-            myDict["Today's letter"]['r'] = 1
-            myDict["Today's letter"]['s'] = 1
-            myDict["Today's letter"]['t'] = 1
-            myDict["Today's letter"]['u'] = 1
-            myDict["Today's letter"]['v'] = 1
-            myDict["Today's letter"]['w'] = 1
-            myDict["Today's letter"]['x'] = 1
-            myDict["Today's letter"]['y'] = 1
-            myDict["Today's letter"]['z'] = 1
-        with open('DailyData.json', 'w') as resetfile:
-            resetfile.write(json.dumps(myDict, indent=4))
-            print("WARNING! Daily file has been reset!\n Restarting WhatPulse Alternative")
-            time.sleep(3)
-            os.system("start cmd /c startmain.pyw")
-            quit()
-    else:    
-        with open('DailyData.json', encoding='utf8') as resetfile: # Evoked by 'Reset' button, resets all daily values;
-            myDict = json.load(resetfile)
-            myDict["Today's LClicks"] = 1
-            myDict["Today's RClicks"] = 1
-            myDict["Today's MClicks"] = 1
-            myDict["Today's Scrolls"] = 1
-            myDict["Today's KeyPress"] = 1
-            myDict["Today's letter"]['a'] = 1
-            myDict["Today's letter"]['b'] = 1
-            myDict["Today's letter"]['c'] = 1
-            myDict["Today's letter"]['d'] = 1
-            myDict["Today's letter"]['e'] = 1
-            myDict["Today's letter"]['f'] = 1
-            myDict["Today's letter"]['g'] = 1
-            myDict["Today's letter"]['h'] = 1
-            myDict["Today's letter"]['i'] = 1
-            myDict["Today's letter"]['j'] = 1
-            myDict["Today's letter"]['k'] = 1
-            myDict["Today's letter"]['l'] = 1
-            myDict["Today's letter"]['m'] = 1
-            myDict["Today's letter"]['n'] = 1
-            myDict["Today's letter"]['o'] = 1
-            myDict["Today's letter"]['p'] = 1
-            myDict["Today's letter"]['q'] = 1
-            myDict["Today's letter"]['r'] = 1
-            myDict["Today's letter"]['s'] = 1
-            myDict["Today's letter"]['t'] = 1
-            myDict["Today's letter"]['u'] = 1
-            myDict["Today's letter"]['v'] = 1
-            myDict["Today's letter"]['w'] = 1
-            myDict["Today's letter"]['x'] = 1
-            myDict["Today's letter"]['y'] = 1
-            myDict["Today's letter"]['z'] = 1
-        with open('DailyData.json', 'w') as resetfile:
-            resetfile.write(json.dumps(myDict, indent=4))
-            print("WARNING! Daily file has been reset!\nRestarting WhatPulse Alternative")
-            time.sleep(1)
-            os.system("start cmd /c startmain.pyw")
-            quit()
+def DailyShowCase():
+    with open("DailyData.json") as File:  # read the data from the file and displays it in popup window;
+        Data = json.loads(File.read())
+        File.close()
+        Foxtrot = Data["Today's Letters"]
+        Charlie = (dict(sorted(Foxtrot.items(), key=lambda val: val[1], reverse=True)))
+        Juliett = ("\n".join("{!r}: {!r},".format(k.upper(), v) for k, v in Charlie.items()))
+        Tango = sub(",", "", Juliett)
+        Letters = sub("'", "", Tango)
+        sg.PopupScrolled("Today's\nLetters\nUsage", f"{Letters}", title="Letters' Statistics",
+                         background_color="grey", grab_anywhere=True, no_titlebar=True, font=16, auto_close=True,
+                         auto_close_duration=30,  size=(12, 15))
+
+def ShowCase():
+    with open("Data.json") as File:  # read the data from the file and displays it in pop window;
+        Data = json.loads(File.read())
+        File.close()
+        Foxtrot = Data["Letters"]
+        Charlie = (dict(sorted(Foxtrot.items(), key=lambda val: val[1], reverse=True)))
+        Juliett = ("\n".join("{!r}: {!r},".format(k.upper(), v) for k, v in Charlie.items()))
+        Tango = sub(",", "", Juliett)
+        Letters = sub("'", "", Tango)
+        sg.PopupScrolled("Lifetime\nLetters\nUsage", f"{Letters}", title="Letters' Statistics",
+                         background_color="grey", grab_anywhere=True, no_titlebar=True, font=16, auto_close=True,
+                         auto_close_duration=30, size=(12, 15))
+
+def File():
+    with open('DailyData.json', encoding='utf8') as resetfile:  # Evoked by 'Reset' button, resets all daily values;
+        myDict = json.load(resetfile)
+        myDict["Today's LClicks"] = 0
+        myDict["Today's RClicks"] = 0
+        myDict["Today's MClicks"] = 0
+        myDict["Today's Scrolls"] = 0
+        myDict["Today's KeyPress"] = 0
+        myDict["Today's Letters"]['a'] = 0
+        myDict["Today's Letters"]['b'] = 0
+        myDict["Today's Letters"]['c'] = 0
+        myDict["Today's Letters"]['d'] = 0
+        myDict["Today's Letters"]['e'] = 0
+        myDict["Today's Letters"]['f'] = 0
+        myDict["Today's Letters"]['g'] = 0
+        myDict["Today's Letters"]['h'] = 0
+        myDict["Today's Letters"]['i'] = 0
+        myDict["Today's Letters"]['j'] = 0
+        myDict["Today's Letters"]['k'] = 0
+        myDict["Today's Letters"]['l'] = 0
+        myDict["Today's Letters"]['m'] = 0
+        myDict["Today's Letters"]['n'] = 0
+        myDict["Today's Letters"]['o'] = 0
+        myDict["Today's Letters"]['p'] = 0
+        myDict["Today's Letters"]['q'] = 0
+        myDict["Today's Letters"]['r'] = 0
+        myDict["Today's Letters"]['s'] = 0
+        myDict["Today's Letters"]['t'] = 0
+        myDict["Today's Letters"]['u'] = 0
+        myDict["Today's Letters"]['v'] = 0
+        myDict["Today's Letters"]['w'] = 0
+        myDict["Today's Letters"]['x'] = 0
+        myDict["Today's Letters"]['y'] = 0
+        myDict["Today's Letters"]['z'] = 0
+    with open('DailyData.json', 'w') as resetfile:
+        resetfile.write(json.dumps(myDict, indent=4))
+        resetfile.flush()
+        resetfile.close()
+        print("WARNING! Daily file has been reset!\nRestarting WhatPulse Alternative")
+        os.startfile("main.pyw")
+
+
+def DateAndFile():
+    datevar = date.today()
+    todaydate = str(datevar)
+    with open('DailyData.json', encoding='utf8') as loaddate:
+        myDict = json.load(loaddate)
+        datescope = myDict["Date"]
+        if todaydate != datescope: # If Date is not concurrent, Updates Date key to today and resets all daily values to 1;
+            with open('DailyData.json', encoding='utf8') as datereset:
+                myDictDate = json.load(datereset)
+                myDictDate["Date"] = todaydate
+            with open('DailyData.json', 'w') as datereset:
+                datereset.write(json.dumps(myDictDate, indent=4))
+                datereset.flush()
+                datereset.close()
+            File()
+        else:
+            File()
+
+
+
+
